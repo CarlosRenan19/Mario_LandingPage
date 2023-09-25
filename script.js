d459f522c9b8d851a13b703f758a65a4c62b2d0a
Renan <r3n4ngamer14@gmail.com>
2023-09-25 11:09:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from('main img:nth-child(1)', 1, { opacity: 0, y: 300, delay: 0 });
+gsap.from('.main__info p', 1, { opacity: 0, y: 300, delay: 0.5 });
+gsap.from('.main__info div', 1, { opacity: 0, y: 300, delay: 0.7 });
+gsap.from('main img:nth-child(2)', 1, { opacity: 0, x: 300, delay: 0.5 });
